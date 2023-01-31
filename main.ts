613d93dd538474708c247f7f160d88f2d62e05cf
DigitalSwitchblade2178 <122555681+DigitalSwitchblade2178@users.noreply.github.com>
2023-01-31 11:42:08 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onGesture(Gesture.Shake, function () {
+    Reset()
+})
 function Update_Score () {
     OLED.clear()
     OLED.writeStringNewLine("Player A" + Player_A)
@@ -8,15 +11,18 @@ function Update_Score () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds" + Rounds)
 }
+function Reset () {
+    OLED.init(128, 64)
+    Player_A = 0
+    Player_B = 0
+    Ties = 0
+    Rounds = 0
+    OLED.writeStringNewLine("Feel up for a game?")
+    basic.pause(2000)
+    Update_Score()
+}
 let Rounds = 0
 let Ties = 0
 let Player_B = 0
 let Player_A = 0
-OLED.init(128, 64)
-Player_A = 0
-Player_B = 0
-Ties = 0
-Rounds = 0
-OLED.writeStringNewLine("Feel up for a game?")
-basic.pause(2000)
-Update_Score()
+Reset()
